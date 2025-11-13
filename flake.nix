0b011fe8defed5a615834bd1bb8c48b0950cfc5f
ilyass <mohamed-ilyass.elmoudene@theodo.com@gmail.com>
2025-11-13 15:19:39 +0100

    Add initial flake.nix and flake.lock for Spring Kafka project

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "A flake for a spring kafka project";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          maven
+          jdk21
+        ];
+      };
+    };
+}
