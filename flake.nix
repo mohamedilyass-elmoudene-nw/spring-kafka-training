3d617e2f282c38506012f990c25848e0867118a9
ilyass <mohamed-ilyass.elmoudene@theodo.com@gmail.com>
2025-11-13 15:30:48 +0100

    Enhance flake.nix to support multiple systems and add minikube to the development shell environment

--- flake.nix
+++ flake.nix
@@ -3,21 +3,21 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, ... }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        inherit system;
-      };
-    in
-    {
-      devShells.${system}.default = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          maven
-          jdk21
-        ];
-      };
-    };
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            maven
+            jdk21
+            minikube
+          ];
+        };
+      });
 }
