7c7bba4ec0ee02e736c5c4c47edef91a7615f6ab
ilyass <mohamed-ilyass.elmoudene@theodo.com@gmail.com>
2025-11-26 17:01:57 +0100

    Add Kafka consumer implementation with Spring Boot; include Taskfile configuration for running the consumer and necessary Maven dependencies.

---
+++ consumer/src/main/java/com/consumerkafka/consumer/KafkaConsumerService.java
@@ -0,0 +1,16 @@
+package com.consumerkafka.consumer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class KafkaConsumerService {
+
+    @KafkaListener(topics = "train-date", groupId = "train-data-consumer-group")
+    public void consume(String message) {
+        log.info("Received message: {}", message);
+    }
+}
+
