d3b4ae6752772b7c931917232bdcee0ac6fb7d8e
ilyass <mohamed-ilyass.elmoudene@theodo.com@gmail.com>
2025-12-02 09:31:40 +0100

    Remove unused imports from ConsumerApplication.java to clean up the codebase.

--- consumer/src/main/java/com/consumerkafka/consumer/ConsumerApplication.java
+++ consumer/src/main/java/com/consumerkafka/consumer/ConsumerApplication.java
@@ -6,8 +6,6 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.kafka.support.converter.JsonMessageConverter;
-import org.springframework.kafka.support.converter.RecordMessageConverter;
 
 @SpringBootApplication
 public class ConsumerApplication {
